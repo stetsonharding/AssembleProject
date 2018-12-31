e8b1906c65d4600cca80ce7a38db7e164605f78f
Stetson Harding <stetson2001@msn.com>
2018-12-31 05:45:59 -0800

    added the title, author, and the button function for table2

--- Code.js
+++ Code.js
@@ -1,4 +1,5 @@
 $(document).ready(function (e) {
+    //button one handling
     $("#tableButton").click(function () {
         // Iterating table rows
         $("#table tr").each(function (i, row) {
@@ -9,8 +10,7 @@ $(document).ready(function (e) {
                 else {
                     $(row).show();
                 }
-           }
-
+            }
         });
         // Changing button state
         if ($("#tableButton").html() == "Show More") {
@@ -19,15 +19,38 @@ $(document).ready(function (e) {
         else {
             $("#tableButton").html("Show More")
         }
-
-
     });
 
+    //button two handling
+    $("#tableButtonRow2").click(function () {
+        // Iterating table rows
+        $("#table2 tr").each(function (i, row) {
+            if (i != 0) {
+                if ($("#tableButtonRow2").html() == "Show Less") {
+                    $(row).hide();
+                }
+                else {
+                    $(row).show();
+                }
+            }
+        });
+        // Changing button state
+        if ($("#tableButtonRow2").html() == "Show More") {
+            $("#tableButtonRow2").html("Show Less");
+        }
+        else {
+            $("#tableButtonRow2").html("Show More")
+        }
+    });
 
+    //getting jason
     $.getJSON("https://www.googleapis.com/books/v1/volumes?q=tech", function (json) {
         var maxColumns = 4;
+        //calling my table(s) and placing table row(s), inside the tables.
         var tableRow = $("#table").append("<tr> </tr>");
         var tableRowTwo = $("#table2").append("<tr></tr>");
+        //calling my json function, and getting all the items inside the json and placeing json items inside "items" variable
+        //sorting titles ****QUESTION****
         var items = json["items"];
         items.sort(function (a, b) {
             if (a.volumeInfo.title == b.volumeInfo.title) {
@@ -40,28 +63,43 @@ $(document).ready(function (e) {
                 return 1;
             }
         });
+        //for each title in json items place the title into the assigned row depenging on the if statement
         $.each(items, function (i, item) {
-
             var firstChar = item.volumeInfo.title[0].toLowerCase();
             if (firstChar >= 'a' && firstChar <= 'm') {
                 tableRow = $("#table tr:last");
                 tableRow.append(
-                "<td>" +
-                "<div class='card'>" +
-                        "<img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' />" +
+                    "<td>" +
+                    "<div class='card'>" +
+                    //gets json image and place the image inside the last tr
+                    "<img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' />" +
                     "<div class='card-body'>" +
-                       "<h5 class='card-title'> "+ item.volumeInfo.title.substr(0, 17) + "</h5>" +
-                       "<p class='card-text'>"+ item.volumeInfo.authors[0] + "</p>" +
+                    //gets title of the book
+                    "<h5 class='card-title'> " + item.volumeInfo.title.substr(0, 17) + "</h5>" +
+                    //gets author of the book
+                    "<p class='card-text'>" + item.volumeInfo.authors[0] + "</p>" +
                     "</div>" +
-                "</div>" +
-                "</td>");
+                    "</div>" +
+                    "</td>");
+                //if the last td size is more than 4, create another row
                 if ($("#table tr:last td").length == maxColumns) {
                     $("#table").append("<tr class='tableResult'></tr>")
                 }
             }
             else {
+                //find the last tr in table 2 and place image, title, and author in it.
+                //If the td is more than 4, create another row.
                 tableRowTwo = $("#table2 tr:last");
-                tableRowTwo.append("<td><img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' /></td>");
+                tableRowTwo.append(
+                    "<td>" +
+                    "<div class='card'>" +
+                    "<img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' />" +
+                    "<div class='card-body'>" +
+                    "<h5 class='card-title'> " + item.volumeInfo.title.substr(0, 17) + "</h5>" +
+                    "<p class='card-text'>" + item.volumeInfo.authors[0].substr(0, 17) + "</p>" +
+                    "</div>" +
+                    "</div>" +
+                    "</td>");
                 if ($("#table2 tr:last td").length == maxColumns) {
                     $("#table2").append("<tr class='tableResult'></tr>")
                 }
