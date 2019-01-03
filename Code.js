498030e5d6f23c3ad62df48fc9384a2e4c448746
Stetson Harding <stetson2001@msn.com>
2019-01-02 20:36:17 -0800

    created functions and started footer

--- Code.js
+++ Code.js
@@ -1,55 +1,88 @@
+const maxColumns = 4;
+const maxLinkColumns = 3;
+function insertBookInRow(table, item) {
+    var author = item.volumeInfo.authors != null && item.volumeInfo.authors.length > 0 ? item.volumeInfo.authors[0].substr(0, 17) : "";
+    var title = item.volumeInfo.title != null ? item.volumeInfo.title.substr(0, 17) : "";
+    var smallThumbnail = item.volumeInfo.imageLinks.smallThumbnail;
+
+    //if the last td size is more than 4, create another row
+    if (table.find("tr:last td").length == maxColumns) {
+        //create new row if >4 td
+        table.append("<tr class='tableResult'></tr>");
+    }
+    else {
+        // show button if table2 td is >5
+        // $('tableButton').style.visibility = 'visible';
+    }
+    let tableRow = table.find("tr:last");
+    tableRow.append(
+        "<td>" +
+        "<div class='card'>" +
+        //gets json image and place the image inside the last tr
+        "<img class ='img-responsive' src='" + smallThumbnail + "' />" +
+        "<div class='card-body'>" +
+        //gets title of the book
+        "<h5 class='card-title'> " + title + "</h5>" +
+        //gets author of the book
+        "<p class='card-text'>" + author + "</p>" +
+        "</div>" +
+        "</div>" +
+        "</td>");
+    }
+
+    function insertLinkIntoFooter(table, item) {
+        var title = item.volumeInfo.title != null ? item.volumeInfo.title.substr(0, 17) : "";
+        var footerLink = item.volumeInfo.previewLink;
+    
+        //if the last td size is more than 3, create another row
+        if (table.find("tr:last td").length == maxLinkColumns) {
+            //create new row if >3 td
+            table.append("<tr class='tableResult'></tr>");
+        }
+     
+        let tableRow = table.find("tr:last");
+        tableRow.append(
+             //footer
+                "<td>" +
+                "<a href='" + item.volumeInfo.previewLink + "'>" + item.volumeInfo.title + "</a>" +
+                "</td>");
+        }
+    
+
+function UpdateButtonState(table, button) {
+    // Iterating table rows
+    table.find("tr").each(function (i, row) {
+        if (i != 0) {
+            if (button.html() == "Show Less") {
+                $(row).hide();
+            }
+            else {
+                $(row).show();
+            }
+        }
+    });
+    // Changing button state
+    if (button.html() == "Show More") {
+        button.html("Show Less");
+    }
+    else {
+        button.html("Show More")
+    }
+}
+
 $(document).ready(function (e) {
     //button one handling
     $("#tableButton").click(function () {
-        // Iterating table rows
-        $("#table tr").each(function (i, row) {
-            if (i != 0) {
-                if ($("#tableButton").html() == "Show Less") {
-                    $(row).hide();
-                }
-                else {
-                    $(row).show();
-                }
-            }
-        });
-        // Changing button state
-        if ($("#tableButton").html() == "Show More") {
-            $("#tableButton").html("Show Less");
-        }
-        else {
-            $("#tableButton").html("Show More")
-        }
+        UpdateButtonState($("#table"), $(this));
     });
     //button two handling
     $("#tableButtonRow2").click(function () {
-        // Iterating table rows
-        $("#table2 tr").each(function (i, row) {
-            if (i != 0) {
-                if ($("#tableButtonRow2").html() == "Show Less") {
-                    $(row).hide();
-                }
-                else {
-                    $(row).show();
-                }
-            }
-        });
-        // Changing button state
-        if ($("#tableButtonRow2").html() == "Show More") {
-            $("#tableButtonRow2").html("Show Less");
-        }
-        else {
-            $("#tableButtonRow2").html("Show More")
-        }
+        UpdateButtonState($("#table2"), $(this));
     });
     //getting jason
     $.getJSON("https://www.googleapis.com/books/v1/volumes?q=tech", function (json) {
-        var maxColumns = 4;
-        //calling my table(s) and placing table row(s), inside the tables.
-        var tableRow = $("#table").append("<tr> </tr>");
-        var tableRowTwo = $("#table2").append("<tr></tr>");
-
         //calling my json function, and getting all the items inside the json and placeing json items inside "items" variable
-        //sorting titles ****QUESTION****
+        //sorting titles 
         var items = json["items"];
         items.sort(function (a, b) {
             if (a.volumeInfo.title == b.volumeInfo.title) {
@@ -62,60 +95,22 @@ $(document).ready(function (e) {
                 return 1;
             }
         });
+        var footerLink = $("#footerTable");
+        var firstChar = null;
+
         //for each item in json items place the item into the assigned row depenging on the if statement
         $.each(items, function (i, item) {
-            var firstChar = item.volumeInfo.title[0].toLowerCase();
+            firstChar = item.volumeInfo.title[0].toLowerCase();
             if (firstChar >= 'a' && firstChar <= 'm') {
-                tableRow = $("#table tr:last");
-                tableRow.append(
-                    "<td>" +
-                    "<div class='card'>" +
-                    //gets json image and place the image inside the last tr
-                    "<img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' />" +
-                    "<div class='card-body'>" +
-                    //gets title of the book
-                    "<h5 class='card-title'> " + item.volumeInfo.title.substr(0, 17) + "</h5>" +
-                    //gets author of the book
-                    "<p class='card-text'>" + item.volumeInfo.authors[0] + "</p>" +
-                    "</div>" +
-                    "</div>" +
-                    "</td>");
-                //if the last td size is more than 4, create another row
-                if ($("#table tr:last td").length == maxColumns) {
-                    $("#table").append("<tr class='tableResult'></tr>")
-                }
+
+                insertBookInRow($("#table"), item);
             }
             else {
-                //find the last tr in table 2 and place image, title, and author in it.
-                //If the td is more than 4, create another row.
-                tableRowTwo = $("#table2 tr:last");
-                tableRowTwo.append(
-                    "<td>" +
-                    "<div class='card'>" +
-                    "<img class ='img-responsive' src='" + item.volumeInfo.imageLinks.smallThumbnail + "' />" +
-                    "<div class='card-body'>" +
-                    "<h5 class='card-title'> " + item.volumeInfo.title.substr(0, 17) + "</h5>" +
-                    "<p class='card-text'>" + item.volumeInfo.authors[0].substr(0, 17) + "</p>" +
-                    "</div>" +
-                    "</div>" +
-                    "</td>");
-                if ($("#table2 tr:last td").length == maxColumns) {
-                    document.getElementById('tableButtonRow2').style.visibility = 'hidden';
-                    $("#table2").append("<tr class='tableResult'></tr>")
-                }
-                else {
-                    document.getElementById('tableButtonRow2').style.visibility = 'visible';
-                }
 
+                insertBookInRow($("#table2"), item);
             }
-
             //footer
-            var footerLink = $("#footerUnorderedList");
-            footerLink.append(
-                "<li>" +
-                "<a href='" + item.volumeInfo.previewLink + "'>" + item.volumeInfo.title + "</a>" +
-                "</li>");
-
+            insertLinkIntoFooter(footerLink,item);
 
         });
 
